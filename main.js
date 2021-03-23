9513f4c2557245ebabdc19b99a5179b25c9bb8ba
Roy <b.roynaim@gmail.com>
2021-03-23 16:20:19 +0100

    bonus immagine

---
+++ main.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue(
+  {
+  el: '#root',
+  data: {
+    message: 'Hello Cal',
+    img: './img/penguin.jpg'
+  },
+  }
+);
